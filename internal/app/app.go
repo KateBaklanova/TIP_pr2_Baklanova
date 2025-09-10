86e3abf1db66bbf790d4213538ebf95a8ccdd9ca
Kate <ggkate12321@gmail.com>
2025-09-10 17:17:20 +0300

    PZ2: updates project with exercise

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,18 +1,12 @@
 package app
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 
+	"github.com/KateBaklanova/myapp/internal/app/handlers"
 	"github.com/KateBaklanova/myapp/utils"
 )
-
-type pingResp struct {
-	Status string `json:"status"`
-	Time   string `json:"time"`
-}
 
 func withRequestID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -31,11 +25,6 @@ func Run() {
 
 	handler := withRequestID(mux)
 
-	utils.LogInfo("Server is starting on :8080")
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		utils.LogError("server error: " + err.Error())
-	}
-
 	// Корневой маршрут
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		utils.LogRequest(r)
@@ -44,17 +33,15 @@ func Run() {
 	})
 
 	// Пример JSON-ручки: /ping
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/ping", handlers.Ping)
+
+	mux.HandleFunc("/fail", func(w http.ResponseWriter, r *http.Request) {
 		utils.LogRequest(r)
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		_ = json.NewEncoder(w).Encode(pingResp{
-			Status: "ok",
-			Time:   time.Now().UTC().Format(time.RFC3339),
-		})
+		utils.WriteErr(w, http.StatusBadRequest, "bad_request_example")
 	})
 
 	utils.LogInfo("Server is starting on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		utils.LogError("server error: " + err.Error())
 	}
 
